aa0230cb44fc77b9918d1db1ed2f5b8ed68b120a
unknown <iidoido@gmail.com>
2016-01-30 15:57:32 +0200

    FIX navbar on settings screen

--- scripts/settings-window.js
+++ scripts/settings-window.js
@@ -1,20 +1,17 @@
 $(document).ready(function() {
-  
-  loadActiveSettings();
 
-  function loadActiveSettings() {
-    var activeSettingsTab = $("#left-navbar li").find(".active");
-    var view_part_name = $(activeSettingsTab).attr("href").slice(1);
+    viewparts.load("core-settings", $("#settings"));
 
-    viewparts.load(view_part_name, $("#settings"));
-  }
-
-    $("#left-navbar li").click(loadActiveSettings);
+    $('body').on('click', '#left-navbar li a', function() {
+      var selectedSettingsTab = $(this).attr("href").slice(1);
+      viewparts.load(selectedSettingsTab, $("#settings"));
+    });
 
     $('#selectDemo').on('change', function (e) {
     var optionSelected = $("option:selected", this).text();
     settings.set(this.id, optionSelected);
     settings.save();
   });
+
 
 });
